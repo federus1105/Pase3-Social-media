7bfa8b765e83ccee1526b6ac6f3a626a19d758e5
federus1105 <federusrudi@gmail.com>
2025-09-30 23:06:03 +0700

    feat(login & register): feature login and register use jwt and hash password

---
+++ internals/repositories/auth.repository.go
@@ -0,0 +1,57 @@
+package repositories
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/federus1105/socialmedia/internals/models"
+	"github.com/federus1105/socialmedia/internals/pkg"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type AuthRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewAuthRepository(db *pgxpool.Pool) *AuthRepository {
+	return &AuthRepository{db: db}
+}
+
+func (r *AuthRepository) Register(ctx context.Context, user models.UserRegister) (models.UserRegister, error) {
+	// Hash password
+	hc := pkg.NewHashConfig()
+	hc.UseRecommended()
+	hashedPassword, err := hc.GenHash(user.Password)
+	if err != nil {
+		log.Println("Error hashing password:", err)
+		return models.UserRegister{}, err
+	}
+
+	sql := `INSERT INTO users (email, password) VALUES ($1, $2) RETURNING userid, email, password`
+	values := []any{user.Email, hashedPassword}
+
+	var newUser models.UserRegister
+	err = r.db.QueryRow(ctx, sql, values...).Scan(&newUser.Id, &newUser.Email, &newUser.Password)
+	if err != nil {
+		log.Println("Failed to insert into users: ", err.Error())
+		return models.UserRegister{}, err
+	}
+
+	return newUser, nil
+}
+
+func (a *AuthRepository) Login(rctx context.Context, email string) (models.UserAuth, error) {
+	sql := `SELECT userid, email, password FROM users WHERE email = $1`
+
+	var user models.UserAuth
+	if err := a.db.QueryRow(rctx, sql, email).Scan(&user.Id, &user.Email, &user.Password); err != nil {
+		if err == pgx.ErrNoRows {
+			return models.UserAuth{}, errors.New("user not found")
+		}
+		log.Println("Internal Server Error.\nCause: ", err.Error())
+		return models.UserAuth{}, err
+	}
+	return user, nil
+}
